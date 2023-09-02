1d942a1045e484c7829c2d648647372a8e55bde3
Gwen <50772474+SGiygas@users.noreply.github.com>
2023-09-02 22:22:43 +0200

    Delete obj directory

--- obj/Debug/net46/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace BrcCustomCharacters
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "BrcCustomCharacters";
-        public const string PLUGIN_NAME = "BrcCustomCharacters";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
