3558dcca67b52740d9e0a66abaeb6d200fa7ee2e
Gwen <50772474+SGiygas@users.noreply.github.com>
2023-10-04 20:19:05 +0200

    Delete CrewBoom Plugin/obj directory

--- CrewBoom Plugin/obj/Debug/net46/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace CrewBoom
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "Crew Boom";
-        public const string PLUGIN_NAME = "Crew Boom";
-        public const string PLUGIN_VERSION = "2.3.0";
-    }
-}
